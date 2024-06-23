ae4cc9ea7ec8f4a9ecfd7d8c2f3da7b35bc1211f
mengbin92 <mengbin1992@outlook.com>
2024-06-23 16:02:22 +0800

    fix(check key before save):

--- internal/data/ca.go
+++ internal/data/ca.go
@@ -16,6 +16,14 @@ type caRepo struct {
 	data *Data
 }
 
+func (r *caRepo) isExists(ctx context.Context, key string) (bool, error) {
+	exists, err := r.data.rdb.Exists(ctx, key).Result()
+	if err != nil {
+		return false, fmt.Errorf("check if key: %s exists error: %s", key, err.Error())
+	}
+	return exists == 1, nil
+}
+
 func (r *caRepo) GetCert(ctx context.Context, common string) (string, error) {
 	return r.data.rdb.Get(ctx, fmt.Sprintf("cert-%s", common)).Result()
 }
@@ -33,10 +41,24 @@ func (r *caRepo) GetPrivateKey(ctx context.Context, common string) (string, erro
 }
 
 func (r *caRepo) SavePrivateKey(ctx context.Context, common, privateKey string) error {
+	exist, err := r.isExists(ctx, fmt.Sprintf("private-%s", common))
+	if err != nil {
+		return err
+	}
+	if exist {
+		return fmt.Errorf("key: private-%s exists", common)
+	}
 	return r.data.rdb.Set(ctx, fmt.Sprintf("private-%s", common), privateKey, 0).Err()
 }
 
 func (r *caRepo) SaveCert(ctx context.Context, common, cert string) error {
+	exist, err := r.isExists(ctx, fmt.Sprintf("cert-%s", common))
+	if err != nil {
+		return err
+	}
+	if exist {
+		return fmt.Errorf("key: cert-%s exists", common)
+	}
 	return r.data.rdb.Set(ctx, fmt.Sprintf("cert-%s", common), cert, 0).Err()
 }
 
@@ -57,5 +79,12 @@ func (r *caRepo) GetRootCert(ctx context.Context, common string) (string, error)
 }
 
 func (r *caRepo) SaveRootCert(ctx context.Context, common, cert string) error {
+	exist, err := r.isExists(ctx, fmt.Sprintf("root-%s", common))
+	if err != nil {
+		return err
+	}
+	if exist {
+		return fmt.Errorf("key: root-%s exists", common)
+	}
 	return r.data.rdb.Set(ctx, fmt.Sprintf("root-%s", common), cert, 0).Err()
 }
